c5d4c3ef5b100b08bee1be657d3c90438c66fdce
JustineAmber <justineamberhill@gmail.com>
2020-11-04 18:55:59 +0100

    Updates

---
+++ precache-manifest.a280160f199fd8aa37c7cc59ab3a4929.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d202a0835fc5ba6c3569e50135b0762",
+    "url": "/country-information-app/index.html"
+  },
+  {
+    "revision": "30ca1084cf4efff3c293",
+    "url": "/country-information-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "99b097638fef64b6b2dc",
+    "url": "/country-information-app/static/js/2.f1e40fb5.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/country-information-app/static/js/2.f1e40fb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30ca1084cf4efff3c293",
+    "url": "/country-information-app/static/js/main.2088e4a6.chunk.js"
+  },
+  {
+    "revision": "040fa1b26a3bbb1d9206",
+    "url": "/country-information-app/static/js/runtime-main.e2365ed1.js"
+  },
+  {
+    "revision": "8f8df061d3b00e487b50abd3f349def1",
+    "url": "/country-information-app/static/media/norway.8f8df061.jpg"
+  }
+]);
